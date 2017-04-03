2c06602dd7f6342b527ef5caeda0178720ef9648
Kasper Nurminen <kasper@Valtteris-iMac.local>
2017-04-03 13:25:20 +0300

    Added criticality-function

--- graphite_beacon/handlers/slack.py
+++ graphite_beacon/handlers/slack.py
@@ -1,4 +1,5 @@
 import json
+import requests
 
 from tornado import httpclient as hc
 from tornado import gen
@@ -34,28 +35,56 @@ class SlackHandler(AbstractHandler):
         self.username = self.options.get('username')
         self.client = hc.AsyncHTTPClient()
 
-    def get_message(self, level, alert, value, target=None, ntype=None, rule=None):  # pylint: disable=unused-argument
+    def get_message(self, level, criticality, alert, value, target=None, ntype=None, rule=None):  # pylint: disable=unused-argument
         msg_type = 'slack' if ntype == 'graphite' else 'short'
         tmpl = TEMPLATES[ntype][msg_type]
         return tmpl.generate(
-            level=level, reactor=self.reactor, alert=alert, value=value, target=target).strip()
+            level=level, reactor=self.reactor, alert=alert, value=value, target=target, criticality=criticality).strip()
+
+
+    def get_alert_criticality(self, level, alert, value, target=None, ntype=None, rule=None):
+        try:
+            criticality = -1
+            username = str(self.reactor.options.get('auth_username'))
+            password = str(self.reactor.options.get('auth_password'))
+            try:
+                id_org, metric = target.split(".")
+            except Exception:
+                return -1
+
+            if metric == "message_in":            
+                activity_data = requests.get("https://"+username+":"+password+"@stats.valopilkkupalvelu.fi/render/?target=summarize(stats.counters.suti."+id_org+".keep_alive_ok.count%2C'100min','sum',true)&from=-100min&format=json").json()
+               
+                if len(activity_data) == 0 or activity_data[0]['datapoints'][-1][0] == None:
+                    return -2
+            try:
+                criticality_data = requests.get("https://"+username+":"+password+"@stats.valopilkkupalvelu.fi/render/?target=summarize(stats.counters.valopilkku.orders.VP."+id_org+"*.request.count%2C'60min','sum',true)&from=-60min&format=json").json()
+            except Exception:
+                return -1
+
+            if len(criticality_data) > 0:
+                criticality = criticality_data[0]['datapoints'][-1][0]
+
+            return criticality
+        except Exception:
+            return -1
 
     @gen.coroutine
     def notify(self, level, *args, **kwargs):
         LOGGER.debug("Handler (%s) %s", self.name, level)
-
-        message = self.get_message(level, *args, **kwargs)
-        data = dict()
-        data['username'] = self.username
-        data['text'] = message
-        data['icon_emoji'] = self.emoji.get(level, ':warning:')
-        if self.channel:
-            data['channel'] = self.channel
-
-        body = json.dumps(data)
-        yield self.client.fetch(
-            self.webhook,
-            method='POST',
-            headers={'Content-Type': 'application/json'},
-            body=body
-        )
+        criticality = self.get_alert_criticality(level, *args, **kwargs)       
+        if criticality > -2:
+            message = self.get_message(level, criticality,  *args, **kwargs)
+            data = dict()
+            data['username'] = self.username
+            data['text'] = message
+            data['icon_emoji'] = self.emoji.get(level, ':warning:')
+            if self.channel:
+                data['channel'] = self.channel
+            body = json.dumps(data)
+            yield self.client.fetch(
+                self.webhook,
+                method='POST',
+                headers={'Content-Type': 'application/json'},
+                body=body
+            )
